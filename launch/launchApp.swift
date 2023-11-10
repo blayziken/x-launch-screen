52fbe7a23cdceef92a6f30c546b55b52863a9d5d
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2023-11-10 10:27:36 +0100

    Initial Commit

---
+++ launch/launchApp.swift
@@ -0,0 +1,17 @@
+//
+//  launchApp.swift
+//  launch
+//
+//  Created by Blaze on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct launchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
